264054b71dc3be4e83df1b70ae5aa90f525ebdfe
Christopher Wallis <chriswallis@chriswallis-macbookpro.roam.corp.google.com>
2018-12-24 13:52:03 -0700

    remove extras, for splitText use splitting.js

--- src/all.ts
+++ src/all.ts
@@ -2,6 +2,5 @@ import { addPlugin } from './lib/core/plugins';
 import { waapiPlugin } from './web';
 
 export * from './main';
-export * from './extras';
 
 addPlugin(waapiPlugin);
--- src/extras/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './random';
-export * from './shuffle';
-export * from './split-text';
